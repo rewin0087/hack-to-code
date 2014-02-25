7c6eac29b103649bfebc56911843fcced07456b0
Rewin0087 <erwin_rulezzz87@yahoo.com>
2014-02-24 20:50:04 -0800

    added ruby test file

---
+++ ruby.rb
@@ -0,0 +1,62 @@
+def palindrum (word)
+  n = word
+  rev = ""
+  i = n.length
+  
+  while i >= 0 do
+    rev += n[i].to_s
+    i-=1
+  end
+  
+  if n == rev
+  	puts "palindrum"
+  else
+  	puts "not a palindrum"
+  end
+  
+  puts "word is " + rev
+end
+
+def fibonacci (n)
+		first_number = 0
+		second_number = 1
+		total = 0
+		
+		for i in 1...n
+			total = first_number + second_number
+			first_number = second_number
+			second_number = total
+		end
+		
+		puts total
+end
+
+
+def pigLatin(word)
+	alpha = ('a'..'z').to_a
+	vowels = %w[a e i o u]
+	consonants = Alpha - Vowels
+
+	w = ""
+	if vowels.include?(word[0]) && word[1] == word[2]
+		w = word[2..-1] + word[0..0] + 'way'
+	elsif vowels.include?(word[0])
+		w = word[1..-1] + word[0..0] + 'way'
+	elsif consonants.include?(word[0]) && 
+		consonants.include?(word[1])
+		w = word[2..-1] + word[0..1] + 'ay'
+	elsif word[0..1] == "qu" 
+		w = word[2..-1]+"quay"
+	elsif word[0..2] == "squ"
+	 	w = word[3..-1]+"squay"
+	else consonants.include?(word[0])
+		w = word[1..-1] + word[0..0] + 'ay'
+	end
+	
+	return w
+end
+
+text = "pig banana trash happy duck glove egg inbox eight"
+puts text.split.map(&method(:pigLatin)).join(' ')
+text = "pig"
+puts pigLatin(text)
